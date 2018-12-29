db19dd11411a8f744194536d52cdf95ad71c3278
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2018-12-29 19:13:44 +0100

    Allow checking zip files

--- SimpleCodeTester-Backend/src/main/java/me/ialistannen/simplecodetester/backend/endpoints/TestRunEndpoint.java
+++ SimpleCodeTester-Backend/src/main/java/me/ialistannen/simplecodetester/backend/endpoints/TestRunEndpoint.java
@@ -4,9 +4,12 @@ import static java.util.stream.Collectors.toList;
 
 import java.io.IOException;
 import java.security.Principal;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -18,9 +21,9 @@ import me.ialistannen.simplecodetester.backend.exception.CompilationFailedExcept
 import me.ialistannen.simplecodetester.backend.services.checkrunning.CheckRunnerService;
 import me.ialistannen.simplecodetester.backend.services.checks.CodeCheckService;
 import me.ialistannen.simplecodetester.submission.ImmutableSubmission;
-import me.ialistannen.simplecodetester.submission.ImmutableSubmission.Builder;
 import me.ialistannen.simplecodetester.submission.Submission;
 import me.ialistannen.simplecodetester.util.ClassParsingUtil;
+import me.ialistannen.simplecodetester.util.StringOutputStream;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
@@ -66,6 +69,52 @@ public class TestRunEndpoint {
     }
   }
 
+  /**
+   * Test an uploaded zip file.
+   *
+   * @param file the uploaded zip file
+   * @param user the user that requested it
+   * @return the test result
+   */
+  @PostMapping("/test/zip")
+  public ResponseEntity<Object> testZipFile(@RequestBody MultipartFile file,
+      @NotNull Principal user) {
+    Map<String, String> files = new HashMap<>();
+
+    if (file == null) {
+      return ResponseEntity.badRequest().body(Map.of("error", "No file given!"));
+    }
+
+    try (ZipInputStream zipInputStream = new ZipInputStream(file.getInputStream())) {
+      ZipEntry entry;
+      while ((entry = zipInputStream.getNextEntry()) != null) {
+        if (entry.isDirectory()) {
+          continue;
+        }
+        if (!entry.getName().endsWith(".java")) {
+          return ResponseEntity.badRequest().body(Map.of(
+              "error", String.format("File '%s' does not end with '.java'", entry.getName())
+          ));
+        }
+
+        StringOutputStream stringOutputStream = new StringOutputStream();
+        byte[] buffer = new byte[1024 * 6];
+        int readLength;
+        while ((readLength = zipInputStream.read(buffer)) >= 0) {
+          stringOutputStream.write(buffer, 0, readLength);
+        }
+
+        files.put(entry.getName(), stringOutputStream.toString());
+      }
+
+      return testMultipleFiles(files, user.getName());
+    } catch (IOException e) {
+      log.info("Error extracting file for user '{}'", user.getName(), e);
+      return ResponseEntity.badRequest()
+          .body(Map.of("error", e.getMessage()));
+    }
+  }
+
   @PostMapping("/test/multiple")
   public ResponseEntity<Object> testMultipleFiles(@NotNull Principal user,
       HttpServletRequest request) {
@@ -82,7 +131,7 @@ public class TestRunEndpoint {
     }
 
     try {
-      Builder builder = ImmutableSubmission.builder();
+      Map<String, String> files = new HashMap<>();
 
       for (Entry<String, List<MultipartFile>> entry : fileMap.entrySet()) {
         for (MultipartFile file : entry.getValue()) {
@@ -90,18 +139,35 @@ public class TestRunEndpoint {
           String packageName = ClassParsingUtil.getPackage(source).map(s -> s + ".")
               .orElse("");
 
-          builder.putFiles(packageName + file.getName(), source);
+          files.put(packageName + file.getName(), source);
         }
       }
 
-      Submission submission = builder.build();
-
-      return test(user.getName(), submission);
+      return testMultipleFiles(files, user.getName());
     } catch (IOException e) {
       log.warn("Error fetching files", e);
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
           .body(Map.of("error", "Error fetching files."));
     }
+  }
+
+  /**
+   * Tests multiple files.
+   *
+   * @param files a map from file name to file content
+   * @param userId the id of the user
+   * @return the resulting response
+   */
+  private ResponseEntity<Object> testMultipleFiles(Map<String, String> files, String userId) {
+    if (files.isEmpty()) {
+      return ResponseEntity.badRequest().body(Map.of("error", "No files given."));
+    }
+
+    Submission submission = ImmutableSubmission.builder()
+        .putAllFiles(files)
+        .build();
+
+    return test(userId, submission);
   }
 
   private Submission fileToSubmission(String source) {
