68b40c10ec324ce0ed7962b2dcb80d6e10f83f41
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-01-01 17:26:21 +0100

    Configure CORS

--- SimpleCodeTester-Backend/src/main/java/me/ialistannen/simplecodetester/backend/security/SecurityJavaConfig.java
+++ SimpleCodeTester-Backend/src/main/java/me/ialistannen/simplecodetester/backend/security/SecurityJavaConfig.java
@@ -1,12 +1,15 @@
 package me.ialistannen.simplecodetester.backend.security;
 
+import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import org.jose4j.jwt.consumer.JwtConsumer;
 import org.jose4j.jwt.consumer.JwtConsumerBuilder;
 import org.jose4j.keys.HmacKey;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +17,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @Configuration
@@ -45,6 +52,7 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
+        .cors().and()
         .csrf().disable()
         .exceptionHandling()
         .authenticationEntryPoint((request, response, authException) ->
@@ -62,5 +70,28 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
         .antMatchers("/login").permitAll()
         .antMatchers("/admin/**").hasRole("ADMIN")
         .anyRequest().authenticated();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource(AllowedOrigins allowedOrigins) {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.addAllowedMethod(HttpMethod.OPTIONS);
+    configuration.addAllowedMethod(HttpMethod.GET);
+    configuration.addAllowedMethod(HttpMethod.HEAD);
+    configuration.addAllowedMethod(HttpMethod.POST);
+    configuration.addAllowedMethod(HttpMethod.DELETE);
+    configuration.addAllowedHeader("Authorization");
+    configuration.setAllowedOrigins(allowedOrigins.getAllowedOrigins());
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+
+  @Component
+  @ConfigurationProperties("cors")
+  @lombok.Value
+  public static class AllowedOrigins {
+
+    private List<String> allowedOrigins;
   }
 }
