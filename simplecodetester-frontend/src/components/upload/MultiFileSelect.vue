b8a715496965ca8b9085262e57f29d5914e17761
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-02-10 21:21:57 +0100

    Clarify that you can upload source zips

--- simplecodetester-frontend/src/components/upload/MultiFileSelect.vue
+++ simplecodetester-frontend/src/components/upload/MultiFileSelect.vue
@@ -2,7 +2,7 @@
   <v-layout align-center justify-center>
     <v-flex xs12>
       <div class="ma-2">
-        <label for="file-upload" class="custom-file-upload">Click me to select file(s)</label>
+        <label for="file-upload" class="custom-file-upload">Click me to select file(s). I can also handle source zips :)</label>
         <input accept=".java, .zip" multiple @input="fileSelected" id="file-upload" type="file">
       </div>
       <v-list>
