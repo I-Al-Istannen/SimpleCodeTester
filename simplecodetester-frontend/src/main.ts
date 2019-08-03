0c7b14c0237539c0d6111af522ccdedf45d5ee43
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-08-03 18:25:03 +0200

    Change default frontend url

--- simplecodetester-frontend/src/main.ts
+++ simplecodetester-frontend/src/main.ts
@@ -11,8 +11,8 @@ Vue.config.productionTip = false
 
 Vue.use(Router)
 
-// Axios.defaults.baseURL = "https://codetester.ialistannen.de"
-Axios.defaults.baseURL = "http://localhost:8081"
+Axios.defaults.baseURL = "https://codetester.ialistannen.de"
+// Axios.defaults.baseURL = "http://localhost:8081"
 
 Axios.interceptors.request.use(
   async request => {
