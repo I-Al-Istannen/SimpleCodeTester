2d911b320b5d733f45418e5bef6c9c9d20c55509
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-07-22 22:13:50 +0200

    [Backend] Java 11!

--- SimpleCodeTester-Lib/src/main/java/me/ialistannen/simplecodetester/compilation/java8/memory/Java8InMemoryCompiler.java
+++ SimpleCodeTester-Lib/src/main/java/me/ialistannen/simplecodetester/compilation/java8/memory/Java8InMemoryCompiler.java
@@ -65,7 +65,7 @@ public class Java8InMemoryCompiler implements Compiler {
 
           diagnostics.put(diagnostic.getSource().getName(), diagnosticMessages);
         },
-        Arrays.asList("-Xlint:all", "-Xlint:-processing", "-Xlint:-serial", "--release=8"),
+        Arrays.asList("-Xlint:all", "-Xlint:-processing", "-Xlint:-serial", "--release=11"),
         null,
         compilationUnits
     )
