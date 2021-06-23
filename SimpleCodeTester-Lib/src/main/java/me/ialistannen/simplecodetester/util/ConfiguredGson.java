325e254b5d1d8e2b358df2c6c0b55dac2b9cfeda
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2021-06-23 15:23:56 +0200

    [lib] Fix serializing of Optional fields

--- SimpleCodeTester-Lib/src/main/java/me/ialistannen/simplecodetester/util/ConfiguredGson.java
+++ SimpleCodeTester-Lib/src/main/java/me/ialistannen/simplecodetester/util/ConfiguredGson.java
@@ -16,10 +16,13 @@ import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.ServiceLoader;
 import lombok.experimental.UtilityClass;
 import me.ialistannen.simplecodetester.checks.CheckResult;
@@ -74,6 +77,7 @@ public class ConfiguredGson {
         .registerTypeAdapterFactory(new GsonAdaptersCheckResult())
         .registerTypeAdapterFactory(new GsonAdaptersSubmission())
         .registerTypeAdapterFactory(new InterleavedMatcherAdapterFactory())
+        .registerTypeAdapterFactory(OptionalTypeAdapter.FACTORY)
         .create();
   }
 
@@ -172,4 +176,50 @@ public class ConfiguredGson {
       );
     }
   }
+
+  // https://stackoverflow.com/a/25078422
+  public static class OptionalTypeAdapter<E> extends TypeAdapter<Optional<E>> {
+
+    public static final TypeAdapterFactory FACTORY = new TypeAdapterFactory() {
+      @SuppressWarnings("unchecked")
+      @Override
+      public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+        Class<T> rawType = (Class<T>) type.getRawType();
+        if (rawType != Optional.class) {
+          return null;
+        }
+        final ParameterizedType parameterizedType = (ParameterizedType) type.getType();
+        final Type actualType = parameterizedType.getActualTypeArguments()[0];
+        final TypeAdapter<?> adapter = gson.getAdapter(TypeToken.get(actualType));
+        return new OptionalTypeAdapter(adapter);
+      }
+    };
+
+    private final TypeAdapter<E> adapter;
+
+    public OptionalTypeAdapter(TypeAdapter<E> adapter) {
+      this.adapter = adapter;
+    }
+
+    @Override
+    public void write(JsonWriter out, Optional<E> value) throws IOException {
+      if(value.isPresent()){
+        adapter.write(out, value.get());
+      } else {
+        out.nullValue();
+      }
+    }
+
+    @Override
+    public Optional<E> read(JsonReader in) throws IOException {
+      final JsonToken peek = in.peek();
+      if(peek != JsonToken.NULL){
+        return Optional.ofNullable(adapter.read(in));
+      }
+
+      in.nextNull();
+      return Optional.empty();
+    }
+  }
+
 }
